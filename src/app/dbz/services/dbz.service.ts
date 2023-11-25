3c3101f4334ac2312e2dda26b6f1727db78f9cf6
Jorge Enrique Cayetano Arevalo (Platino247) <dt.programadorPL2@upch.pe>
2023-11-25 14:26:28 -0500

    continue learning 24-11-23

---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,39 @@
+import { Character } from './../interfaces/character.interface';
+import { Injectable } from '@angular/core';
+import { v4 as uuid } from 'uuid';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DbzService {
+  public characters: Character[] = [
+    {
+      id: uuid(),
+      name: 'Triangle',
+      power: 1000,
+    },
+    {
+      id: uuid(),
+      name: 'Circle',
+      power: 9500,
+    },
+    {
+      id: uuid(),
+      name: 'Square',
+      power: 7500,
+    },
+  ];
+
+  orNewCharacter(character: Character): void {
+    const newCharacter: Character = {
+      id: uuid(),
+      ...character,
+    };
+
+    this.characters.push(newCharacter);
+  }
+
+  onDelete(index: number): void {
+    this.characters.splice(index, 1);
+  }
+}
